dbc8772acd65b0ac6d59ef11d3b9c3cff6f25e48
Omid Zamzami <omidzamzami27@gmail.com>
2026-08-19 19:59:19 +0330

    Add the PushEvent feature

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ base_url = f"https://api.github.com/users/{username}/events"
 
 response = requests.get(base_url, timeout=10)
 events = response.json()
+
 for event in events:
     event_type = event['type']
+    payload = event.get("payload", {})
 
     match event_type:
         case "PushEvent":
-            pass
+            if "size" in payload:
+                commit_count = payload['size']
+            elif "commits" in payload:
+                commit_count = len(payload['commits'])
+            else:
+                commit_count = 1
+
+            username_repo = event['repo']['name']
+            print(f"Pushed {commit_count} commit(s) to {username_repo}")
+
         case "IssuesEvent":
             pass
         case "WatchEvent":
